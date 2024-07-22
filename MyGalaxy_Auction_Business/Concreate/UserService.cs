e01d965e7d2e31b65886c1946bda5b2296f546f6
alialtunar <altunarali284@gmail.com>
2024-07-22 21:39:29 +0300

    fixing

--- MyGalaxy_Auction_Business/Concreate/UserService.cs
+++ MyGalaxy_Auction_Business/Concreate/UserService.cs
@@ -1,4 +1,6 @@
 ﻿using MyGalaxy_Auction_Business.Abstract;
+using MyGalaxy_Auction_Business.Dtos;
+using MyGalaxy_Auction_Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,16 @@ using System.Threading.Tasks;
 
 namespace MyGalaxy_Auction_Business.Concreate
 {
-    public class UserService:IUserService
+    public class UserService : IUserService
     {
+        public Task<ApiResponse> Login(LoginRequestDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResponse> Register(RegisterRequestDTO model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
